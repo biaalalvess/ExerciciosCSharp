307935b1457b113544f281a8cfcbba70e8a1949c
Beatriz Alves <bibialvespaula123@gmail.com>
2026-04-28 16:42:25 -0300

    exercicio finalizado

---
+++ Exercicio035/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information//
+int Media;
+float PT, CG, MT, mediaTotal;
+Console.WriteLine("Nota de Portugues: ");
+PT = float.Parse(Console.ReadLine());
+Console.WriteLine("Nota de conhecimento geral: ");
+CG = float.Parse(Console.ReadLine());
+Console.WriteLine("Nota de Matematica: ");
+MT = float.Parse(Console.ReadLine());
+mediaTotal = (PT + CG + MT) / 3;
+if (mediaTotal >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (mediaTotal < 7)
+{
+    Console.WriteLine("Reprovado");
+}
+
+
+
+
